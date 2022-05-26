9a0e0c885ed9830db07c8f52ab946a813ba61b24
Ali.ghobadi <93571384+Ali25gh@users.noreply.github.com>
2022-05-26 13:18:55 +0430

    front-end:end navbar

--- front-end/script/navbar.js
+++ front-end/script/navbar.js
@@ -1,8 +1,12 @@
-//search
 let search = document.getElementById("search")
 let search_nav = document.getElementById("search1")
 let intro_text = document.getElementById("header__intro")
 let search_input = document.getElementById("search__input")
+let login = document.getElementById("login")
+let rigester = document.getElementById("rigester")
+let login_after = document.getElementById("login_after")
+let login_before = document.getElementById("login_before")
+//search
 function search_open() {
     search.style.width = "30rem";
     search.style.opacity = "1";
@@ -13,9 +17,34 @@ function search_open2() {
     intro_text.style.opacity = "0";
 }
 //login
-document.getElementById("login").addEventListener("mouseover",function(){
-    document.getElementById("icon_navbar").style.transform="translate(33px, 7px)";
-    });
-    document.getElementById("login").addEventListener("mouseout",function(){
-        document.getElementById("icon_navbar").style.transform="translate(0, 0)";
-        });
+document.getElementById("login").addEventListener("mouseover", function () {
+    document.getElementById("icon_navbar").style.transform = "translate(33px, 7px)";
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        document.getElementById("icon_navbar").style.transform = "translate(0, 0)";
+    }
+});
+document.getElementById("login").addEventListener("mouseout", function () {
+    document.getElementById("icon_navbar").style.transform = "translate(0, 0)";
+});
+function login_animation() {
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        login.style.display = "block";
+        rigester.style.display = "block";
+
+        setTimeout(() => {
+            login.style.transform = "translate(-10px,25px)";
+            rigester.style.width = "6rem";
+            rigester.style.transform = "translate(10px, 26px)";
+            login_after.style.display = "block";
+            login_before.style.display = "block";
+        }, 10)
+        setTimeout(() => {
+            login.style.display = "none";
+            rigester.style.display = "none";
+            login_after.style.display = "none";
+            login_before.style.display = "none";
+        }, 6000)
+    }
+
+}
+

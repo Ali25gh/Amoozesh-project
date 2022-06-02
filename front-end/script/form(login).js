04fe6db21cfbec38d4b698f5ee53f92f34a826a7
Ali.ghobadi <93571384+Ali25gh@users.noreply.github.com>
2022-06-03 00:43:45 +0430

    front-end:end login page

--- front-end/script/form(login).js
+++ front-end/script/form(login).js
@@ -10,10 +10,18 @@ const radio_text_2 = document.getElementById("radio_text_2");
 const radio_hand = document.getElementById("radio_hand");
 const radio_hand_2 = document.getElementById("radio_hand_2");
 const famale_radio = document.getElementById("female");
+const background = document.getElementById("form__background");
 const male = document.getElementById("male");
 const error = document.getElementById("err");
 let btn = document.getElementById("btn_form")
 let div_disable = document.getElementById("div_diasabler")
+document.getElementById("glass").addEventListener("click", function () {
+    document.getElementById("form__background").style.transform = "scale(4.5)";
+    if (window.matchMedia("(max-width: 992px)").matches) {
+        document.getElementById("form__background").style.transform = "scale(3.37)"
+
+    }
+});
 function glass_animation() {
     glass.style.animation = "swing 1s ease";
     glass.style.backgroundColor = "white"
@@ -102,12 +110,35 @@ document.addEventListener("DOMContentLoaded", function () {
     btn.style.opacity = ".5";
     btn.style.pointerEvents = "none";
     btn.disabled = true;
+    let random = Math.floor(Math.random() * 5) + 1;
+    switch (random) {
+        case 1:
+            background.src = "../front-end/images/background-login1.jpg"
+            break;
+
+        case 2:
+            background.src = "../front-end/images/background-login2.jpg"
+            break;
+        case 3:
+            background.src = "../front-end/images/background-login3.jpg"
+            break;
+        case 4:
+            background.src = "../front-end/images/background-login4.jpg"
+            break;
+        case 5:
+            background.src = "../front-end/images/background-login5.jpg"
+            break;
+    }
 })
 
 btn.addEventListener("click", function () {
     if ((email.value == "") || (fullname.value == "")) {
         error.style.right = "35.9rem";
         error.style.top = "27rem";
+        if (window.matchMedia("(max-width: 530px)").matches) {
+            error.style.right = "16.9rem";
+            error.style.top = "28rem";
+        }
         error.innerHTML = "فیلد ها را پر کنید";
         error.style.opacity = "1";
         setTimeout(() => {
@@ -117,6 +148,10 @@ btn.addEventListener("click", function () {
     else if (fullname.value.length < 4) {
         error.style.right = "10.9rem";
         error.style.top = "20rem";
+        if (window.matchMedia("(max-width: 530px)").matches) {
+            error.style.right = "2.9rem";
+            error.style.top = "20rem";
+        }
         error.style.opacity = "0";
         error.innerHTML = 'کاراکتر های نام کاربری باید حداقل ۴ تا باشند <i class="fas fa-window-close" style="color: #8a010f;padding-right: 0.2rem;animation: wave-hand 3.6s .3s;"></i>';
         setTimeout(() => {
@@ -129,6 +164,10 @@ btn.addEventListener("click", function () {
     else if (email.value.length < 8 || email.value.length > 15) {
         error.style.right = "10.9rem";
         error.style.top = "20rem";
+        if (window.matchMedia("(max-width: 530px)").matches) {
+            error.style.right = "2.9rem";
+            error.style.top = "20rem";
+        }
         error.style.opacity = "0";
         error.innerHTML = 'کاراکتر های رمز باید بین ۸ تا ۱۵ باشند <i class="fas fa-window-close" style="color: red;padding-right: 0.2rem;animation: wave-hand 3.6s .3s;"></i>';
         setTimeout(() => {
@@ -140,16 +179,21 @@ btn.addEventListener("click", function () {
     }
     else {
         error.style.opacity = "1";
-        error.style.right = "34.9rem";
+        error.style.right = "37.9rem";
         error.style.top = "27rem";
-        error.innerHTML = "فرم ارسال شد" + '<i class="fas fa-check" style="color: green;padding-right: 0.5rem;animation: wave-hand 2.6s .3s;"></i>';   
+        if (window.matchMedia("(max-width: 530px)").matches) {
+            error.style.right = "14.9rem";
+            error.style.top = "20rem";
+        }
+        error.innerHTML = "انجام شد" + '<i class="fas fa-check" style="color: green;padding-right: 0.5rem;animation: wave-hand 2.6s .3s;"></i>';
     }
-    if (error.style.right == "34.9rem") {
+    if (error.style.right == "37.9rem" || error.style.right == "14.9rem") {
         setTimeout(() => {
-            glass.style.animation = "fade-out .76s ease";
+            btn.style.animation = "fade-out .76s ease";
+            btn.style.pointerEvents = "none";
         }, 3100)
         setTimeout(() => {
-            glass.style.display = "none";
+            btn.style.opacity = "0";
         }, 3600)
     }
 })
